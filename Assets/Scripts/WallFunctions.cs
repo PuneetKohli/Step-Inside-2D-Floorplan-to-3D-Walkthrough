8e843349a778cdfbaa33b2fdd4028b0c04802e6d
Aditya More <Aditya More>
2016-03-18 12:50:33 +0530

    added libraries and 3d wall scripts

---
+++ Assets/Scripts/WallFunctions.cs
@@ -0,0 +1,229 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Poly2Tri;
+
+public class WallFunctions : MonoBehaviour {
+
+	public float thickness = 0.2f;
+	public float height = 5f;
+	private Vector3 _start_pt;
+	private Vector3 _end_pt;
+	private float angle = 0;
+	private float length = 0;
+	private Dictionary<Vector3, int> vertexIndices = new Dictionary<Vector3, int> ();
+	private List<Vector3> new_verts = new List<Vector3>();
+	private List<int> new_tris = new List<int> ();
+	private int new_vert_count = 0;
+	private int hole_start_index = 4;
+	private int hole_end_index = 8;
+	public float Angle {
+		get {
+			return this.angle;
+		}
+		set {
+			angle = value;
+		}
+	}
+
+	public Vector3 Start_pt {
+		get {
+			return this._start_pt;
+		}
+		set {
+			_start_pt = value;
+		}
+	}
+
+	public Vector3 End_pt {
+		get {
+			return this._end_pt;
+		}
+		set {
+			_end_pt = value;
+		}
+	}
+		
+
+	public void generateWall(Vector3 start, Vector3 end) {
+
+		Start_pt = start;
+		End_pt = end;
+		// get angle between direction vector and x axis
+		Vector3 wall_vector = end - start;
+
+		float angle = Vector3.Angle (wall_vector, new Vector3(1, 0, 0));
+		Vector3 cross = Vector3.Cross (wall_vector, new Vector3 (1, 0, 0));
+		if (cross.y > 0) angle = -angle;
+		//this.Angle = angle;
+
+		//get wall length
+		length = wall_vector.magnitude;
+
+		//create mesh
+		gameObject.AddComponent<MeshFilter>();
+		gameObject.AddComponent<MeshRenderer>();
+		Mesh mesh = this.GetComponent<MeshFilter> ().mesh;
+		List<Vector3> verts = new List<Vector3> ();
+		verts.Add(new Vector3( -thickness / 2, 0f, 0f));
+		verts.Add(new Vector3(-thickness / 2, height, 0f));
+		verts.Add(new Vector3(-thickness / 2, height, length));
+		verts.Add(new Vector3(-thickness / 2, 0f, length));
+
+		mesh.vertices = verts.ToArray ();;
+		mesh.triangles = new int[]{0, 2, 1, 0, 3, 2};
+
+
+		//extrude mesh
+		mesh = this.extrudeWall(mesh);
+
+		//adjust wall parameters
+
+		//assign material
+		Material newMat = Resources.Load("meshgen material", typeof(Material)) as Material;
+		MeshRenderer renderer = this.GetComponent<MeshRenderer> ();
+		renderer.materials[0] = newMat;
+
+		//transform mesh to position and angle
+		this.transform.RotateAround (new Vector3 (0, 0, 0), new Vector3 (0, 1, 0), 90 + angle); 
+		this.transform.position = start;
+
+	}
+
+	public void addHole() {
+		Mesh mesh = GetComponent<MeshFilter> ().mesh;
+
+		List<Vector2> rectanglePoints = new List<Vector2> ();
+		List<Vector2> holePoints = new List<Vector2> ();
+
+		rectanglePoints.Add (new Vector2 (0, 0));
+		rectanglePoints.Add (new Vector2 (0, height));
+		rectanglePoints.Add (new Vector2 (length, height));
+		rectanglePoints.Add (new Vector2 (length, 0));
+
+		holePoints.Add (new Vector2 (length / 3, height /3));
+		holePoints.Add (new Vector2 (length / 3, 2 * height /3));
+		holePoints.Add (new Vector2 (2* length / 3, 2 * height /3));
+		holePoints.Add (new Vector2 (2 * length / 3, height /3));
+
+		Polygon Rectangle = createPoly (rectanglePoints.ToArray());
+		Polygon Hole = createPoly (holePoints.ToArray());
+
+
+		Rectangle.AddHole (Hole);
+		P2T.Triangulate (Rectangle);
+		//Debug.Log ("here");
+		for (int i = 0; i < Rectangle.Triangles.Count; i++)
+			for (int j = 0; j < 3; j++) {
+				TriangulationPoint tpt = Rectangle.Triangles [i].Points[j];
+				Vector3 pt = new Vector3 ( -thickness / 2,(float) tpt.Y, (float) tpt.X);
+				new_tris.Add (vertexIndices [pt]);
+			}
+		mesh.Clear ();
+		mesh.vertices = new_verts.ToArray ();
+		mesh.triangles = new_tris.ToArray ();
+
+		Debug.Log ("First vertex " + mesh.vertices [0]);
+
+		mesh = this.extrudeWall(mesh);
+	}
+
+	private Polygon createPoly(Vector2[] points) {
+		List<PolygonPoint> polyPoints = new List<PolygonPoint> ();
+		for (int i = 0; i < points.Length; i++) {
+			polyPoints.Add (new PolygonPoint (points [i].x, points [i].y));
+			Vector3 pt = new Vector3 ( -thickness / 2, points [i].y, points [i].x);
+			new_verts.Add (pt);
+			vertexIndices.Add (pt, new_vert_count);
+			new_vert_count++;
+		}
+		Polygon P = new Polygon (polyPoints);
+		return P;
+	}
+
+	private Mesh extrudeWall(Mesh mesh) {
+		//duplicate vertices
+		Vector3[] orignal_vertices = mesh.vertices;
+		Vector3[] back_vertices = new Vector3[orignal_vertices.Length];
+		Vector3[] mid_vertices = new Vector3[4];
+
+		Vector3 thickness_vector = new Vector3 ( thickness, 0, 0);
+		for (int i = 0; i < orignal_vertices.Length; i++)
+			back_vertices [i] = orignal_vertices [i] + thickness_vector;
+
+		Vector3 mid_vector = new Vector3 (thickness / 2, 0, 0);
+		for (int i = 0; i < mid_vertices.Length; i++)
+			mid_vertices [i] = orignal_vertices [i] + mid_vector;
+		
+
+		int ovl = orignal_vertices.Length;
+
+		//generate inverted back triangles
+		int[] orignal_triangles = mesh.triangles;
+
+		int[] back_triangles = new int[orignal_triangles.Length];
+		for (int i = 0; i < orignal_triangles.Length; i+=3) {
+			back_triangles [i] = orignal_triangles [i] + ovl;
+			back_triangles [i + 1] = orignal_triangles [i + 2] + ovl;
+			back_triangles [i + 2] = orignal_triangles [i + 1] + ovl;
+		}
+
+		//hole triangles
+		int[] hole_triangles = new int[(hole_end_index - hole_start_index) * 2 * 3]; //two triangles for each edge with 3 values each
+
+		// triangles for hex faces (subset of bevel triangles)
+		int[] hex_triangles = new int[8 * 2 * 3];//8 quads with 2 triangles with 3 values each
+		for (int i = 0; i < 4; i++) {
+			int first = i;
+			int second = (i + 1) % 4;
+			int[] quad_tri = quadTriangles (first, first + 2 * ovl, second + 2 * ovl, second);
+			quad_tri.CopyTo (hex_triangles, 2 * 2 * 3 * i); //offset of 2 quads
+			quad_tri = quadTriangles(first  + 2 * ovl, first + ovl, second + ovl, second + 2 * ovl);
+			quad_tri.CopyTo (hex_triangles, 2 * 2 * 3 * i + 2 * 3);
+		}
+		int count = 0;
+		for (int i = hole_start_index; i < hole_end_index; i++) {
+			int first = i;
+			int second = hole_start_index + ((count + 1) % (hole_end_index - hole_start_index));
+			int[] quad_tri = quadTriangles (first + ovl, first, second, second + ovl);
+			quad_tri.CopyTo (hole_triangles, 6 * count);
+			count++;
+		}
+
+		//combine arrays 
+		Vector3[] vertices = new Vector3[orignal_vertices.Length + back_vertices.Length + mid_vertices.Length ];
+		orignal_vertices.CopyTo (vertices, 0);
+		back_vertices.CopyTo (vertices, orignal_vertices.Length);
+		mid_vertices.CopyTo (vertices, orignal_vertices.Length + back_vertices.Length);
+
+		int[] triangles = new int[orignal_triangles.Length + back_triangles.Length + hex_triangles.Length + hole_triangles.Length];
+		orignal_triangles.CopyTo (triangles, 0);
+		back_triangles.CopyTo (triangles, orignal_triangles.Length);
+		hex_triangles.CopyTo (triangles, orignal_triangles.Length + back_triangles.Length);
+		hole_triangles.CopyTo (triangles, orignal_triangles.Length + back_triangles.Length + hex_triangles.Length);
+
+		/*Debug.Log (triangles.Length);
+		for (int i = 0; i < triangles.Length; i ++)
+			Debug.Log (triangles[i]);*/
+
+		//add to mesh and return
+		mesh.vertices = vertices;
+		mesh.triangles = triangles;
+		return mesh;
+	}
+
+	int[] quadTriangles (int a, int b, int c, int d) {
+		int[] triangles = {a, c, b, a, d, c};
+		return triangles;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/WallGenerator.cs
@@ -0,0 +1,195 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class WallGenerator : MonoBehaviour {
+
+	public float height = 5;
+	List<Vector3[]> point_pairs = new List<Vector3[]>();
+	public float thickness = 0.2f;
+	private Dictionary<Vector3, List<GameObject>> nodes = new Dictionary<Vector3, List<GameObject>>();
+	private bool alternator = false;
+	private Vector3[][] point_pairs_array = null; 
+	// Use this for initialization
+
+
+
+	Vector3[][] init_point_pairs() {
+		point_pairs.Add (new Vector3[] {new Vector3(0, 0, 0), new Vector3(5, 0, 0) });
+		point_pairs.Add (new Vector3[] {new Vector3(5, 0, 0), new Vector3(5, 0, 2) });
+		point_pairs.Add (new Vector3[] {new Vector3(5, 0, 5), new Vector3(5, 0, 2) });
+		point_pairs.Add (new Vector3[] {new Vector3(5, 0, 5), new Vector3(3, 0, 4) });
+		point_pairs.Add (new Vector3[] {new Vector3(3, 0, 4), new Vector3(1, 0, 5) });
+		point_pairs.Add (new Vector3[] {new Vector3(1, 0, 5), new Vector3(0, 0, 0) });
+		point_pairs.Add (new Vector3[] {new Vector3(3, 0, 2), new Vector3(5, 0, 2) });
+		point_pairs.Add (new Vector3[] {new Vector3(3, 0, 4), new Vector3(3, 0, 2) });
+
+		return point_pairs.ToArray ();
+	}
+	void Start (){
+		//Vector3[][] point_pairs_array = init_point_pairs ();
+		GameObject[] walls = new GameObject[point_pairs_array.Length];
+		for (int i = 0; i < point_pairs_array.Length; i++) {
+			GameObject wall_object = new GameObject ();
+			wall_object.name = "Wall " + i;
+			wall_object.transform.parent = this.transform;
+			walls[i] = wall_object;
+			WallFunctions wall_script = wall_object.AddComponent <WallFunctions>();
+			wall_script.generateWall (point_pairs_array [i][0], point_pairs_array [i][1]);
+
+		}
+
+		//Debug.Log (-3 % walls.Length);
+		//adjust all walls
+		foreach(GameObject wall_object in walls) {
+			addOrUpdate(wall_object.GetComponent<WallFunctions>().Start_pt, wall_object);
+			addOrUpdate(wall_object.GetComponent<WallFunctions>().End_pt, wall_object);
+		}
+
+		List<GameObject>[] node_values = nodes.Values.ToArray ();
+		Vector3[] node_points = nodes.Keys.ToArray ();
+		walls [2].GetComponent<WallFunctions> ().addHole ();
+		for (int i = 0; i < node_values.Count(); i++) {
+			GameObject[] coincident_walls = node_values [i].ToArray (); 
+			for (int j = 0; j < coincident_walls.Length; j++) {
+				Vector3 start = getStart (coincident_walls [j]);
+				Vector3 end = getEnd (coincident_walls [j]);
+				Vector3 otherPoint = start == node_points[i] ? end : start;
+
+				float angle = Vector3.Angle (otherPoint - node_points[i], new Vector3(1, 0, 0));
+				Vector3 cross = Vector3.Cross (otherPoint - node_points[i], new Vector3 (1, 0, 0));
+				if (cross.y < 0) angle = - angle;
+
+
+				coincident_walls [j].GetComponent<WallFunctions> ().Angle = angle;
+			}
+			if (coincident_walls.Length < 3)
+				coincident_walls = coincident_walls.OrderBy (w => w.GetComponent<WallFunctions> ().Angle).ToArray();
+			else 
+				coincident_walls = coincident_walls.OrderByDescending (w => w.GetComponent<WallFunctions> ().Angle).ToArray();
+
+			alternator = false;
+			Debug.Log ("Point : " + node_points [i]);
+			if (coincident_walls.Length > 1) {
+				for (int j = 0; j < coincident_walls.Length; j++) {
+					Debug.Log (coincident_walls [j].name + " : " + coincident_walls [j].GetComponent<WallFunctions>().Angle + ", " + coincident_walls [(j + 1)  % coincident_walls.Length].name + " : " + coincident_walls [(j + 1)  % coincident_walls.Length].GetComponent<WallFunctions>().Angle);
+					alternator = !alternator;
+					adjustShape (coincident_walls [j], coincident_walls [(j + 1) % coincident_walls.Length], node_points [i]);
+				}
+			}
+		}
+
+
+	}
+
+	//draw corner points using gizmos 
+	/*void OnDrawGizmos() {
+		this.point_pairs_array = init_point_pairs();
+		//init_corners ();
+		Gizmos.color = Color.black;
+		for (int i = 0; i < point_pairs_array.Length; i++) {
+			Gizmos.DrawSphere(point_pairs_array[i][0], 0.1f);
+			Gizmos.DrawSphere(point_pairs_array[i][1], 0.1f);
+		}
+			
+	}*/
+	void Awake() {
+		this.point_pairs_array = init_point_pairs();
+	}
+	private void addOrUpdate(Vector3 corner, GameObject wall) {
+		if (nodes.ContainsKey (corner)) {
+			List<GameObject> l = nodes [corner];
+			l.Add (wall);
+			nodes [corner] = l;
+		} else {
+			List<GameObject> l = new List<GameObject>();
+			l.Add(wall);
+			nodes.Add (corner, l);
+		}
+	}
+
+	private void adjustShape(GameObject a, GameObject b, Vector3 point) {
+		float angle = a.GetComponent<WallFunctions> ().Angle - b.GetComponent<WallFunctions> ().Angle;
+		int baseA, baseB;
+
+		//angle adjustments
+		if (angle > 180) {
+			angle = - (angle - 180);
+		}
+
+		if (angle < -180) {
+			angle = - (angle + 180);
+		}
+		Debug.Log (angle);
+
+		Mesh meshA = a.GetComponent<MeshFilter> ().mesh;
+		Mesh meshB = b.GetComponent<MeshFilter> ().mesh;
+
+		Vector3[] vertsA = meshA.vertices;
+		int ovlA = (vertsA.Length - 4) / 2;
+		Vector3[] vertsB = meshB.vertices;
+		int ovlB = (vertsB.Length - 4) / 2;
+
+		float ext = (thickness / 2) / Mathf.Tan (angle * Mathf.Deg2Rad/ 2);
+
+		Debug.Log (ext);
+		//if (Mathf.Abs (angle) > 90)
+		//	ext = -ext;
+	
+		//Debug.Log (a.name + " " + b.name + " : " + ext);
+		bool isStartA = isStart (a, point);
+		bool isStartB = isStart (b, point);
+
+		if (isStartA) {
+			baseA = ovlA;
+		}
+		else 
+			baseA = 0;
+
+		if (!isStartB) {
+			baseB = ovlB;
+		}
+		else 
+			baseB = 0;
+		
+		//subtract positive z direction vector from close-to-angle edge of A
+		Vector3 ext_vector = new Vector3(0, 0, ext);
+
+		if (isStartA) {
+			vertsA [baseA + 0] += ext_vector;
+			vertsA [baseA + 1] += ext_vector;
+		} else {
+			vertsA [baseA + 2] -= ext_vector;
+			vertsA [baseA + 3] -= ext_vector;
+		}
+
+		if (isStartB) {
+			vertsB [baseB + 0] += ext_vector;
+			vertsB [baseB + 1] += ext_vector;
+		} else {
+			vertsB [baseB + 2] -= ext_vector;
+			vertsB [baseB + 3] -= ext_vector;
+		}
+		meshA.vertices = vertsA;
+		meshB.vertices = vertsB;
+
+
+
+
+	}
+	private bool isStart(GameObject a, Vector3 point) {
+		//when coincident edges arent all start edges
+		return (a.GetComponent<WallFunctions> ().Start_pt == point);
+	}
+	private Vector3 getStart(GameObject a) {
+		return a.GetComponent<WallFunctions> ().Start_pt;
+	}
+
+	private Vector3 getEnd(GameObject a) {
+		return a.GetComponent<WallFunctions> ().End_pt;
+	}
+	void Update () {
+		
+	}
+}
